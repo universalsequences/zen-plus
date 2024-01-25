af0334b91a81b2f84d56e46658d07e8aa28ba082
universalsequences <universalsequences@gmail.com>
2024-01-25 10:35:39 -0500

    update contracts

--- src/lib/onchain/contracts.ts
+++ src/lib/onchain/contracts.ts
@@ -3,7 +3,7 @@ type Contracts = {
     [x: string]: `0x${string}`
 }
 export const contracts: Contracts = {
-    DropCreator: "0x2BAe0C13CFF08Ac88ee7bAb9D932e1153B5bE322",
-    MetadataRenderer: "0x3FF7ae46f164d9Aa8B67f5F1f84f5164Fc7eBBa2",
-    ZenModule: "0x4561d6a1331ac88e379ac45867e4fcb4b7ed280e"
+    DropCreator: "0x6E939512124A9f704B8807bEF4B5f76e29ADc12a",
+    MetadataRenderer: "0xe0B12850FE86456acD72381CF3F694295c2ff251",
+    ZenModule: "0x6f76b514578c47d1544bf4718b194a51e648a990"
 };
