9acd89fae6bcfd53913514db287b1eb61e7054c0
universalsequences <universalsequences@gmail.com>
2024-01-25 10:47:31 -0500

    attempting cors

--- src/pages/api/getHTML.tsx
+++ src/pages/api/getHTML.tsx
@@ -5,6 +5,8 @@ import { contracts } from '@/lib/onchain/contracts';
 import type { NextApiRequest, NextApiResponse } from 'next';
 import Web3 from 'web3';
 
+const Cors = require('cors');
+
 // Define your metadata ABI and other ABIs
 
 // Set your contract addresses and Web3 provider
@@ -12,9 +14,28 @@ const web3 = new Web3(new Web3.providers.HttpProvider(`https://goerli.infura.io/
 
 // Cache for storing fetched HTML content
 
+// Import the cors library
+// Initialize the cors middleware
+const cors = Cors({
+    methods: ['GET', 'HEAD', 'POST'], // Set the methods you want to allow
+    origin: 'https://testnet.zora.co', // Replace with the specific hostname you want to allow
+});
+
+// Helper method to initialize and use CORS in your API route
+function runMiddleware(req: any, res: any, fn: any) {
+    return new Promise((resolve, reject) => {
+        fn(req, res, (result: any) => {
+            if (result instanceof Error) {
+                return reject(result);
+            }
+            return resolve(result);
+        });
+    });
+}
+
+
 async function getHTML(contractAddress: string, tokenId: string): Promise<string | null> {
     const contract = new web3.eth.Contract(abi as any, contracts.MetadataRenderer);
-
     try {
         const tokenURI: string = await contract.methods.onchainTokenURI(contractAddress, tokenId).call() as string;
         // Check if tokenURI is base64 encoded, if not, assume it's a URL
@@ -37,6 +58,8 @@ async function getHTML(contractAddress: string, tokenId: string): Promise<string
 }
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    await runMiddleware(req, res, cors);
+
     const { contractAddress, tokenId } = req.query;
 
     if (typeof tokenId !== 'string' || typeof contractAddress !== 'string') {
