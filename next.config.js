938a15da8f0c6be49e4e72a5957eb1e18fb4df46
universalsequences <universalsequences@gmail.com>
2024-03-29 15:28:52 -0400

    update next.config.js to fix build error

--- next.config.js
+++ next.config.js
@@ -3,6 +3,19 @@ const nextConfig = {
      images: {
          domains: ['storage.googleapis.com', 'zequencer.mypinata.cloud'],
   },
+    future: {
+webpack5: true,
+   },
+  webpack(config) {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+      net: false,
+      tls: false,
+    };
+
+    return config;
+  },
 }
 
 module.exports = nextConfig
